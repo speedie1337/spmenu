ad4c468abd8e89d903c2a2208fa0c569813b1a8c
speedie <speedie@duck.com>
2023-04-01 00:13:05 +0200

    oops forgot to change this

--- spmenu.c
+++ spmenu.c
@@ -608,8 +608,14 @@ setupdisplay(void)
     #endif
 
 	// input methods
-	if ((xim = XOpenIM(dpy, NULL, NULL, NULL)) == NULL)
-		die("XOpenIM failed: could not open input device");
+    if ((xim = XOpenIM(dpy, NULL, NULL, NULL)) == NULL) {
+		XSetLocaleModifiers("@im=local");
+		if ((xim = XOpenIM(dpy, NULL, NULL, NULL)) == NULL) {
+			XSetLocaleModifiers("@im=");
+			if ((xim = XOpenIM(dpy, NULL, NULL, NULL)) == NULL)
+				die("XOpenIM failed: could not open input device");
+		}
+	}
 
 	xic = XCreateIC(xim, XNInputStyle, XIMPreeditNothing | XIMStatusNothing,
 	                XNClientWindow, win, XNFocusWindow, win, NULL);
