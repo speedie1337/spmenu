90875c2deca54eb9348d788e23ecd222faaa5fe7
speedie <speedie@duck.com>
2023-03-31 16:05:24 +0200

    we don't need that last if anymore, and we can reduce the first if to a single line

--- libs/mode.c
+++ libs/mode.c
@@ -4,13 +4,7 @@ switchmode(const Arg *arg)
     curMode = !curMode;
     allowkeys = !curMode;
 
-    if (!curMode) {
-        strcpy(modetext, normtext);
-    } else {
-        strcpy(modetext, instext);
-    }
-
-    if (hidemode) strcpy(modetext, "");
+    strcpy(modetext, curMode ? instext : normtext);
 
     drawmenu();
 }
