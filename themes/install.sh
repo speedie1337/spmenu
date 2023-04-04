4a1cc69f7fff9f6316677d74959053b59c39c230
speedie <speedie@duck.com>
2023-04-04 23:47:01 +0200

    some minor changes, fix segmentation fault by removing options spmenu cannot handle using .Xresources
    the options removed can be set in the spmenu.conf

--- themes/install.sh
+++ themes/install.sh
@@ -3,5 +3,5 @@
 # This script installs all the profiles, allowing spmenu to see and choose them.
 CONFDIR="${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p "$CONFDIR/spmenu/profiles/"
-cp ./* legacy/* "$CONFDIR/spmenu/profiles/"
+cp legacy/* "$CONFDIR/spmenu/profiles/"
 rm -f "$CONFDIR/spmenu/profiles/install.sh" "$CONFDIR/spmenu/profiles/README.md" "$CONFDIR/spmenu/profiles/sample.theme"
