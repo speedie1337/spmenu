5f54e20a2bc817be0363c8de21f083dd14c51c96
speedie <speedie@duck.com>
2023-05-01 17:48:21 +0200

    some cleanup, add comments to buildconf

--- libs/mouse.h
+++ libs/mouse.h
@@ -20,38 +20,5 @@ typedef struct {
 	Arg arg;
 } Mouse;
 
-#if USECONFIG
-typedef struct {
-    char *click;
-    unsigned int button;
-} ButtonType;
-
-typedef struct {
-    char *tclick;
-    unsigned int click;
-} ClickType;
-
-static ButtonType btp[] = {
-    { "Left Click",   Button1 },
-    { "Middle Click", Button2 },
-    { "Right Click",  Button3 },
-    { "Scroll Up",    Button4 },
-    { "Scroll Down",  Button5 },
-};
-
-static ClickType ctp[] = {
-    { "clickwindow",  clickwindow },
-    { "clickprompt",  clickprompt },
-    { "clickinput",   clickinput },
-    { "clicklarrow",  clicklarrow },
-    { "clickitem",    clickitem },
-    { "clickselitem", clickselitem },
-    { "clickrarrow",  clickrarrow },
-    { "clicknumber",  clicknumber },
-    { "clickcaps",    clickcaps },
-    { "clickmode",    clickmode },
-};
-#endif
-
 static Mouse cbuttons[256];
 static void buttonpress(XEvent *e);
