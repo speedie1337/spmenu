8ab66132f1d427f01f3e630f6a828b85c5dc9df7
speedie <speedie@duck.com>
2023-05-08 17:29:30 +0200

    add example script for counting entries

---
+++ scripts/examples/spmenu_itemcounter.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+func() {
+    rm -f /tmp/fl
+
+    i=0 cnum=0
+    while true; do
+        printf "%s \033[0;3${cnum}m%d\n" "Item" "$i" >> /tmp/fl
+        i=$((i+1))
+        cnum=$((cnum+1))
+
+        [ "$cnum" -gt "9" ] && cnum="1"
+
+        sleep 1
+    done
+}
+func | spmenu --list-file /tmp/fl --lines 40 --columns 1 --prompt "Updates automatically!"
