72af4b40192d2cf45f5ed06a55eaee88cae1e7b3
speedie <speedie@duck.com>
2023-05-08 12:01:36 +0200

    remove .mk, we don't need them anymore as we don't have makefiles

--- scripts/make/generate-pkg.sh
+++ scripts/make/generate-pkg.sh
@@ -3,9 +3,11 @@ version="$(grep "version : '" meson.build | awk '{ print $3 }' | sed "s/'\"//g; 
 
 rm -f spmenu spmenu-${version}.tar.gz spmenu-${version}.PKGBUILD *.o *zst*
 mkdir -p spmenu-${version}
-cp -rf LICENSE meson.build *.h *.mk *.c scripts/ docs/ libs/ PKGBUILD build.sh spmenu-${version}
+cp -rf LICENSE meson.build *.h *.c scripts/ docs/ libs/ PKGBUILD build.sh spmenu-${version}
 [ -f buildconf ] && cp buildconf spmenu-${version} || :
 [ -f Makefile ] && cp Makefile spmenu-${version} || :
+[ -f host.mk ] && cp host.mk spmenu-${version} || :
+[ -f toggle.mk ] && cp toggle.mk spmenu-${version} || :
 [ -f spmenu.1 ] && cp spmenu.1 spmenu-${version} || :
 tar -cf spmenu-${version}.tar spmenu-${version}
 gzip spmenu-${version}.tar
