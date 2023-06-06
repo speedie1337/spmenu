1ac9211b02b99d9e35cefc5116996635d1ebf66f
speedie <speedie@duck.com>
2023-06-07 01:08:10 +0200

    remove preview.png, it's not used anymore

--- scripts/make/generate-docs.sh
+++ scripts/make/generate-docs.sh
@@ -6,13 +6,11 @@
 [ ! -f spmenu.c ] && printf "You're probably in the wrong directory.\n" && exit 1
 version="$(grep "version : '" meson.build | awk '{ print $3 }' | sed "s/'\"//g; s/\"',//g")"
 printf "%% spmenu(1) ${version} | fancy dynamic menu\n" > .man.md
-grep -v docs/preview.png docs/docs.md >> .man.md
 pandoc --standalone --to man .man.md -o spmenu.1
 pandoc --standalone .man.md -o spmenu.html --metadata title="spmenu man page"
 rm -f .man.md
 
 printf "%% spmenu_run(1) ${version} | \$PATH/.desktop launcher and file manager\n" > .man.md
-grep -v docs/preview.png docs/run-docs.md >> .man.md
 pandoc --standalone --to man .man.md -o spmenu_run.1
 rm -f .man.md
 
