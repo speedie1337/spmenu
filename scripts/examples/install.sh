59d9f63238fc9572a81b5ad5cc3fb612c8e500dc
speedie <speedie@duck.com>
2023-06-17 15:26:51 +0200

    Remove example scripts, move them to spmenu-examples repository

--- scripts/examples/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# install.sh
-# Install all scripts in the current directory.
-#
-# See LICENSE file for copyright and license details.
-[ "$(id -u)" != "0" ] && printf "Run me as root.\n" && exit 1
-cp -f spmenu* /usr/bin
--- scripts/examples/spmenu_16cols.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# spmenu_16cols.sh
-# Prints 16 foreground and 16 background colors
-#
-# See LICENSE file for copyright and license details.
-
-printf "\033[0;31mItem 1\n\033[0;32mItem 2\n\033[0;33mItem 3\n\033[0;34mItem 4\n\033[0;35mItem 5\n\033[0;36mItem 6\n\033[0;37mItem 7\n\033[0;38mItem 8\n\033[1;31mItem 9\n\033[1;32mItem 10\n\033[1;33mItem 11\n\033[1;34mItem 12\n\033[1;35mItem 13\n\033[1;36mItem 14\n\033[1;37mItem 15\n\033[1;38mItem 16\n\
-\033[0;41mItem 1\n\033[0;42mItem 2\n\033[0;43mItem 3\n\033[0;44mItem 4\n\033[0;45mItem 5\n\033[0;46mItem 6\n\033[0;47mItem 7\n\033[0;48mItem 8\n\033[1;41mItem 9\n\033[1;42mItem 10\n\033[1;43mItem 11\n\033[1;44mItem 12\n\033[1;45mItem 13\n\033[1;46mItem 14\n\033[1;47mItem 15\n\033[1;48mItem 16\n" | spmenu "$@"
