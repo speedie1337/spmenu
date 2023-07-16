f2776a39aaa90d53c9dd41fc0e1e5f918a3326ec
speedie <speedie@duck.com>
2023-07-16 18:49:20 +0200

    Declare functions inside arg.h

--- libs/arg.h
+++ libs/arg.h
@@ -7,7 +7,10 @@ typedef union {
     const char *c; // char
 } Arg;
 
-// declare keybind functions
+/* declare keybind functions
+ *
+ * movement
+ */
 static void moveup(Arg *arg);
 static void movedown(Arg *arg);
 static void moveleft(Arg *arg);
@@ -17,31 +20,55 @@ static void movestart(Arg *arg);
 static void movenext(Arg *arg);
 static void moveprev(Arg *arg);
 static void moveitem(Arg *arg);
+static void moveword(Arg *arg);
+static void movecursor(Arg *arg);
+
+/* input */
 static void paste(Arg *arg);
 static void restoresel(Arg *arg);
 static void clear(Arg *arg);
 static void clearins(Arg *arg);
-static void viewhist(Arg *arg);
-static void moveword(Arg *arg);
 static void deleteword(Arg *arg);
-static void movecursor(Arg *arg);
-static void navhistory(Arg *arg);
 static void backspace(Arg *arg);
+static void complete(Arg *arg);
+
+/* history */
+static void viewhist(Arg *arg);
+static void navhistory(Arg *arg);
+
+/* selection */
 static void markitem(Arg *arg);
 static void selectitem(Arg *arg);
-static void quit(Arg *arg);
-static void complete(Arg *arg);
+
+/* image */
 static void setimgsize(Arg *arg);
-static void toggleimg(Arg *arg);
-static void toggleregex(Arg *arg);
 static void defaultimg(Arg *arg);
 static void flipimg(Arg *arg);
 static void setimgpos(Arg *arg);
 static void setimggaps(Arg *arg);
+
+/* lines/columns */
 static void setlines(Arg *arg);
 static void setcolumns(Arg *arg);
+
+/* misc */
 static void spawn(Arg *arg);
-static void togglehighlight(Arg *arg);
-static void setprofile(Arg *arg);
-static void switchmode(Arg *arg);
 static void screenshot(Arg *arg);
+static void switchmode(Arg *arg);
+static void setprofile(Arg *arg);
+static void quit(Arg *arg);
+
+/* toggle */
+static void toggleinput(Arg *arg);
+static void togglelarrow(Arg *arg);
+static void togglerarrow(Arg *arg);
+static void toggleitem(Arg *arg);
+static void toggleprompt(Arg *arg);
+static void togglecaps(Arg *arg);
+static void togglepowerline(Arg *arg);
+static void togglecaret(Arg *arg);
+static void togglematchcount(Arg *arg);
+static void togglemode(Arg *arg);
+static void togglehighlight(Arg *arg);
+static void toggleregex(Arg *arg);
+static void toggleimg(Arg *arg);
